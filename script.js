47c8dd5ad5565c60331ceef6fd1611ac0fa05e7c
Shantanu Kumar Mahanta <121172938+shantanukumar-dev@users.noreply.github.com>
2024-07-01 22:50:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+//JAVASCRIPT Provides 8 Mathematical constants that can be accessed as math properties
+// These are the following constants:-
+console.log(Math.E);
+console.log(Math.PI);
+console.log(Math.SQRT2);
+console.log(Math.SQRT1_2);
+console.log(Math.LN2);
+console.log(Math.LN10);
+console.log(Math.LOG2E);
+console.log(Math.LOG10E);
